d38ce038c9bdb2a7c96613c10c2ecf830645bb76
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2024-10-23 19:49:43 +0000

    conversions

---
+++ 01_Basics/01_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Anuj"
+
+console.log(typeof score)
+console.log(typeof (score)) //Method
+
+let valueinNumber = Number(score)
+//console.log(typeof valueinNumber)
+//console.log(valueinNumber)
+
+// "33" => 33
+// Anuj => Nan
+// ture=> 1; false => 0
+
+let isLoggedIn = "Anuj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ANUJ" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
